5449a7fd5f2dbd3146df2557582eb47f77f7c4cb
Kirillov_Ivan <kirillovio@mail.ru>
2024-03-04 06:01:37 +0300

    add editing the date

--- src/main.py
+++ src/main.py
@@ -17,6 +17,11 @@ for data_operation in reversed(data_cart):
         index_empty_element_list = data_cart.index(data_operation)
         del data_cart[index_empty_element_list]
 
+for data_operation in data_cart:
+    # Удаляю "Т" в дате
+    data_operation['date'] = data_operation['date'].replace('T',' ')
+    # Оставляю только дату и время
+    data_operation['date'] = data_operation['date'][:19]
 
 for data_operation in data_cart:
     print(data_operation)
