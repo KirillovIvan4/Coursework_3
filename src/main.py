9e72d0517634d4dfb0d0ed64a4b4ab71fada85a0
Kirillov_Ivan <kirillovio@mail.ru>
2024-03-04 05:59:25 +0300

    add Deleting canceled transactions

--- src/main.py
+++ src/main.py
@@ -11,5 +11,12 @@ for data_operation in reversed(data_cart):
         index_empty_element_list = data_cart.index(data_operation)
         del data_cart[index_empty_element_list]
 
+# Удаляю отмененые операции
+for data_operation in reversed(data_cart):
+    if data_operation["state"] == "CANCELED":
+        index_empty_element_list = data_cart.index(data_operation)
+        del data_cart[index_empty_element_list]
+
+
 for data_operation in data_cart:
     print(data_operation)
